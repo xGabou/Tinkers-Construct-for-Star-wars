cde400c13a46c9d83890494f5a34646c864dcfd9
KnightMiner <knightminer4@gmail.com>
2025-08-09 18:23:53 -0400

    Increase slots per level for item frame to 3
    Makes it a little more enticing for exploration

--- src/main/java/slimeknights/tconstruct/tools/data/ModifierProvider.java
+++ src/main/java/slimeknights/tconstruct/tools/data/ModifierProvider.java
@@ -495,7 +495,7 @@ public class ModifierProvider extends AbstractModifierProvider implements ICondi
     // helmet
     buildModifier(ModifierIds.respiration).addModule(EnchantmentModule.builder(Enchantments.RESPIRATION).constant());
     buildModifier(ModifierIds.aquaAffinity).addModule(EnchantmentModule.builder(Enchantments.AQUA_AFFINITY).constant()).levelDisplay(ModifierLevelDisplay.NO_LEVELS);
-    buildModifier(TinkerModifiers.itemFrame).addModule(InventoryModule.builder().pattern(pattern("item_frame")).flatLimit(1).slotsPerLevel(1));
+    buildModifier(TinkerModifiers.itemFrame).addModule(InventoryModule.builder().pattern(pattern("item_frame")).flatLimit(1).slotsPerLevel(3));
     buildModifier(ModifierIds.minimap).addModule(InventoryModule.builder().pattern(pattern("map")).filter(TinkerPredicate.MAP).flatLimit(1).slotsPerLevel(3)).addModule(MinimapModule.INSTANCE);
     // chestplate
     buildModifier(ModifierIds.strength).addModule(AttributeModule.builder(Attributes.ATTACK_DAMAGE, Operation.MULTIPLY_TOTAL).slots(armorSlots).eachLevel(0.1f));
