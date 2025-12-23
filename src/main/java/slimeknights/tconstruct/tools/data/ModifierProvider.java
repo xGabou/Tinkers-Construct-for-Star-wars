68636488acba7a433c28ac89063628d6a016d3d2
KnightMiner <knightminer4@gmail.com>
2025-12-23 02:29:28 -0500

    Improve longfall tooltips
    Now shows 100% fall resistance instead of 50%

--- src/main/java/slimeknights/tconstruct/tools/data/ModifierProvider.java
+++ src/main/java/slimeknights/tconstruct/tools/data/ModifierProvider.java
@@ -631,7 +631,9 @@ public class ModifierProvider extends AbstractModifierProvider implements ICondi
       .addModule(new VolatileFlagModule(ToolInventoryCapability.CRAFTING_TABLE));
     // boots
     buildModifier(ModifierIds.depthStrider).addModule(EnchantmentModule.builder(Enchantments.DEPTH_STRIDER).constant());
-    buildModifier(ModifierIds.featherFalling).addModule(ProtectionModule.builder().source(DamageSourcePredicate.tag(TinkerTags.DamageTypes.FALL_PROTECTION)).eachLevel(6.25f));
+    buildModifier(ModifierIds.featherFalling)
+      .addModule(ProtectionModule.builder().source(DamageSourcePredicate.tag(TinkerTags.DamageTypes.FALL_PROTECTION))
+        .toolContext(HasModifierPredicate.hasModifier(ModifierIds.longFall, 1).inverted()).eachLevel(6.25f));
     buildModifier(ModifierIds.longFall)
       .levelDisplay(ModifierLevelDisplay.NO_LEVELS)
       .addModule(ModifierRequirementsModule.builder().requireModifier(ModifierIds.featherFalling, 2).modifierKey(ModifierIds.longFall).build())
